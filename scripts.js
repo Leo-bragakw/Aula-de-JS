5beaa99a10634f66121d4c69b3b2ccf8e0697894
Leo_braga <leojbragafilho@hotmail.com>
2025-07-11 11:40:56 -0300

    aula de sintaxe

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("hello word!")
+// sintaxe correta.
+console.log("leo")
+
+// sintaxe incorreta.
+// con.sole.log("leo")
+
+// No JS o ponto e virgula é opicional.
+//Ex:
+console.log("leo");
+console.log("leo")
