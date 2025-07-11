5bd2abba53b696447fa87491cb3d7b0f98bb4665
Leo_braga <leojbragafilho@hotmail.com>
2025-07-11 17:35:54 -0300

    variavel do tipo boolean (true or false)

--- scripts.js
+++ scripts.js
@@ -1,14 +1,7 @@
-// Number
-console.log (typeof 5)
+// Boolean
+console.log (true)
+console.log (false)
 
-//Inteiro positivo
-console.log (5)
-console.log ("5")
-
-//Inteiro Negativo
-console.log (-5)
-
-// Números reais ou float.
-console.log (7.5)
-//obs: a utilização de "," é entendida como numeros separados por conta disso se usa o "."
-console.log (12.5 / "Leo")
+let isLoading = true
+console.log (isLoading)
+console.log  (typeof isLoading)
