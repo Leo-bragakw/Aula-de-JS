761393f2ff8eaa703f6bc2a7f7b144a661457c53
Leo_braga <leojbragafilho@hotmail.com>
2025-07-11 17:59:18 -0300

    conversão e coerção de tipos de variaveis

--- scripts.js
+++ scripts.js
@@ -1,7 +1,33 @@
-// underfined (indefinido)
-let emptiness
-console.log ("o valor é:", emptiness)
+/* 
+    - Conversão de tipos (type casting ou type conversion):
+      ocorre quando você explicitamente transforma um valor de um tipo para outro. Isso é feito de forma consciente, 
+    usando funções ou métodos especificos para realizar a conversão.
 
-// Null
-let empty = null
-console.log ("O valor é: ", empty)
+   
+*/
+
+console.log ( typeof "9")
+console.log (typeof Number ("9"))
+
+let value = "9"
+console.log ( typeof value)
+console.log (typeof Number (value))
+
+let age = 18
+console.log (typeof age.toString())
+console.log (typeof String(age))
+
+let option = 1
+console.log (typeof option)
+console.log (typeof Boolean(option))
+console.log (Boolean (option))
+
+
+/*
+    - Coerção de tipos:
+      acontece de forma automática (implicitamente). O JavaScript tenta automaticamente converter um dos valores para um tipo
+      compatível antes de realizar a operação.
+*/
+
+console.log ("10" + 5) // o JS converte o número 5 num texto.
+console.log (typeof ("10" + 5))
