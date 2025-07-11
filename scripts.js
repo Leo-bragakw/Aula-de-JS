9e9414e1aa428b2baddad81e10a77a65bff0e5b5
Leo_braga <leojbragafilho@hotmail.com>
2025-07-11 16:04:43 -0300

    aula sobre escopo e uso das variaveis var e let

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
-// criando uma constante (variavel com valor fixo)
-const number = 42
-console.log(number)
+// Escopo é a "região" onde a variavel se encontra (gloal, em bloco ou afins).
+// console.log(user)
+// var user = "leonardo"
+
+// Hoisting
+// var user
+// console.log(user)
+
+
+// o var é sempre içado a um escopo global.
+
+// Escopo global
+var email = "leo@hotmail.com"
+{
+    // escobo de bloco
+    console.log(email)
+}
+
+{
+    var age = 18
+}
+
+console.log(age)
+
+
+// o let respeita sempre o escopo em que foi criado e tambem sempre respeita a hierarquia
+//Ex:
+{
+    let think = "penso logo existo"
+}
+
+console.log(think) // aparece uma mensagem de erro.
+
+//Ex:
+let address = "rua piatã"
+{
+    console.log(address)
+}
